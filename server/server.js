e6327144bc1fe145cfa9a1df24f78339583ecc03
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-27 18:12:46 +0500

    Refactor server.js to use ES module syntax; added dotenv for environment variable support and configured port from process.env

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const express = require('express')
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
-const port =  3000
+const port = process.env.PORT || 3000
+dotenv.config()
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
