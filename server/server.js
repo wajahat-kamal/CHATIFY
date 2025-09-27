c09425caa8500a3f2aedef9932d4408d9c6a4cd1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-27 18:15:26 +0500

    Add MongoDB connection to server.js with error handling; logs connection status on startup.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ const app = express()
 const port = process.env.PORT || 3000
 dotenv.config()
 
-
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("Connect DB");
+}).catch((error) => {
+  console.log("Failedto Connect", error);
+  
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
