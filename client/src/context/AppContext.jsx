2e8e84cbf9abe37b21d95528fd1fc84bf681772c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-30 18:14:41 +0500

    Refactor AppContextProvider to include fetchUser functionality and improve state management; ensure proper context value structure for user, chats, and navigation.

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,27 +4,30 @@ import { dummyUserData } from "../assets/Data";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const {user, setUser} = useState(null)
-  const {chats, setChats} = useState([])
-  const {selectedChat, setSelectedChat} = useState(null)
-  const {theme, setTheme} = useState(localStorage.getItem("theme") || "light")
+  const { user, setUser } = useState(null);
+  const { chats, setChats } = useState([]);
+  const { selectedChat, setSelectedChat } = useState(null);
+  const { theme, setTheme } = useState(
+    localStorage.getItem("theme") || "light"
+  );
 
   const fetchUser = () => {
-    setUser(dummyUserData)
-  }
+    setUser(dummyUserData);
+  };
 
-  const value = {}
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-  return (
-    <AppContext.Provider value={value}>
-      {children}
-    </AppContext.Provider>
-  );
+  const value = {
+    navigate, user, setUser, chats, setChats, selectedChat, setSelectedChat, theme, fetchUser
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => useContext(AppContext);
 
-export default AppContextProvider
+export default AppContextProvider;
