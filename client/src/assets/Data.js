f5964bb8fb73522359b5b3b13a9366b6adad7e1b
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-30 18:12:19 +0500

    Add user state management and navigation in AppContext; implement fetchUser function to set dummy user data.

---
+++ client/src/assets/Data.js
@@ -0,0 +1,8 @@
+export const dummyUserData = {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    password: "2316skrnehohgoiwerhoigjorj",
+    credits: 200,
+  };
+  
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,8 +1,21 @@
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "../assets/Data";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+
+  const navigate = useNavigate()
+
+  const {user, setUser} = useState(null)
+  const {chats, setChats} = useState([])
+  const {selectedChat, setSelectedChat} = useState(null)
+  const {theme, setTheme} = useState(localStorage.getItem("theme") || "light")
+
+  const fetchUser = () => {
+    setUser(dummyUserData)
+  }
+
   const value = {}
 
   return (
