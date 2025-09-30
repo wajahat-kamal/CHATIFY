a449e3d0a64706c89ebb57928b359677197f2065
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-30 18:06:26 +0500

    Add BrowserRouter to main entry point for routing support; wrap App component for improved navigation.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AppContextProvider from "./context/AppContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppContextProvider>
   </React.StrictMode>
 );
