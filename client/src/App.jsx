01b3fcb0c123d93fe23d58712d3b8fb983d08314
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-30 17:53:06 +0500

    Refactor App component layout for improved structure; adjust main content area and ensure proper rendering of routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import {Routes, Route} from "react-router-dom"
 import ChatBox from "./components/ChatBox";
 import Credits from "./pages/Credits";
 import Community from "./pages/Community";
 
 function App() {
   return (
-    <>
-    <div className="dark:bg-gradient-to-b from-[#242124] to-[#00000] dark:text-white">
-      <div className="flex h-screen w-screen">
+    <div className="dark:bg-gradient-to-b from-[#242124] to-black dark:text-white min-h-screen">
+      <div className="flex w-screen h-screen">
         <Sidebar />
-        <Routes>
-          <Route path="/" element={<ChatBox/>}/>
-          <Route path="/credits" element={<Credits/>}/>
-          <Route path="/community" element={<Community/>}/>
-        </Routes>
+        <main className="flex-1 overflow-y-auto">
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </main>
       </div>
-      </div>
-    </>
+    </div>
   );
 }
 
