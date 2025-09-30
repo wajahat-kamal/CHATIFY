b120321b85e2371c0505984496dfcf622e6b5432
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-30 18:26:13 +0500

    Integrate user and chat context into Sidebar component; add state management for search functionality.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../context/AppContext'
 
 function Sidebar() {
+
+  const {user, chats, theme, setTheme, setSelectedChats} = useAppContext()
+  const {search, setSearch} = useState('')
+
   return (
     <div>Sidebar</div>
   )
