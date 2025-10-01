ed757aa7d53d6871ffbc85f7598fc72cd00138df
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-01 21:40:40 +0500

    Implement chat message handling in ChatBox component: integrate context for selected chat and manage message state with useEffect for improved user experience.

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
 
 function ChatBox() {
+  const { selectedChat, theme } = useAppContext();
+
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
   return (
-    <div>ChatBox</div>
-  )
+    <div className="flex justify-between flex-col flex-1 m-5 xl:mx-30 max-md:mt-14 2xl:pr-40">
+      {/* messages */}
+      <div></div>
+
+      {/* chat input */}
+      <form></form>
+    </div>
+  );
 }
 
-export default ChatBox
+export default ChatBox;
