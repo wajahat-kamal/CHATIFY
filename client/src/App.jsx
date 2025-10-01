db842098b050733a9eeb23644f15f6323d67968d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-01 18:00:16 +0500

    Replace menu button with SVG icon in App component; enhance accessibility and styling for improved user experience.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,26 +4,34 @@ import Sidebar from "./components/Sidebar";
 import ChatBox from "./components/ChatBox";
 import Credits from "./pages/Credits";
 import Community from "./pages/Community";
-import { Menu } from "lucide-react";
+import menu from "./assets/menu.svg";
 function App() {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
-    {!isMenuOpen && <button onClick={() => setIsMenuOpen(true)} className="md:hidden block fixed top-5 left-5 bg-purple-600 text-white p-2 rounded-full"><Menu/></button>}
-    <div className="dark:bg-gradient-to-b from-[#242124] to-black dark:text-white min-h-screen">
-      <div className="flex w-screen h-screen">
-        <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        <main className="flex-1 overflow-y-auto">
-          <Routes>
-            <Route path="/" element={<ChatBox />} />
-            <Route path="/credits" element={<Credits />} />
-            <Route path="/community" element={<Community />} />
-          </Routes>
-        </main>
+      {!isMenuOpen && (
+        <img
+          onClick={() => setIsMenuOpen(true)}
+          aria-label="Open Menu"
+          className="md:hidden fixed top-5 left-5 w-9 h-9 not-dark:invert"
+          src={menu}
+          alt="Menu Icon"
+        />
+      )}
+
+      <div className="dark:bg-gradient-to-b from-[#242124] to-black dark:text-white min-h-screen">
+        <div className="flex w-screen h-screen">
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          <main className="flex-1 overflow-y-auto">
+            <Routes>
+              <Route path="/" element={<ChatBox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-    </div>
     </>
   );
 }
