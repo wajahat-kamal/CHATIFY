e0185ccf21bb5cf8706f3a041b8d30b779115204
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-02 10:03:12 +0500

    Update loading state in ChatBox component to true by default and enhance loader animation with staggered delays for improved visual feedback.

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -41,8 +41,8 @@ function ChatBox() {
 
         {loading && (
           <div className="loader flex items-center gap-2">
-            <div className="w-2 h-2 rounded-full bg-gray-500 dark:bg-white animate-bounce [animation-delay:-0.s]"></div>
-            <div className="w-2 h-2 rounded-full bg-gray-500 dark:bg-white animate-bounce [animation-delay:-0.4s]"></div>
+            <div className="w-2 h-2 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-2 h-2 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
             <div className="w-2 h-2 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
           </div>
         )}
