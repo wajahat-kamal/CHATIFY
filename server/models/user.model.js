e4a6444d2eb67a8d727e457768d4397603c3bc89
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-03 22:05:30 +0500

    Enhance user model: add password hashing functionality using bcryptjs and improve schema structure for better data integrity.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,27 +1,33 @@
 import mongoose from "mongoose";
- 
+import bcrypt from "bcryptjs";
+
 const userSchema = new mongoose.Schema({
-   name: {
+  name: {
     type: String,
-    required: true
-   },
-   name: {
+    required: true,
+  },
+  email: {
     type: String,
     required: true,
     unique: true,
-   },
-   password: {
+  },
+  password: {
     type: String,
-    required: true
-   },
-   credits: {
+    required: true,
+  },
+  credits: {
     type: Number,
-    default: 20
-   },
-})
+    default: 20,
+  },
+});
 
-// hashing password before saving
-userSchema.pre()
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 export default User;
