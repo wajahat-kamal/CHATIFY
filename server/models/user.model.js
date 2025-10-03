7678957f4d1c33db2f50355586ea6ce28061bec2
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-03 22:01:26 +0500

    Remove chatbot controller, router, and bot model to streamline the application structure and eliminate unused code.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,20 +1,27 @@
 import mongoose from "mongoose";
  
 const userSchema = new mongoose.Schema({
-    sender: {
-        type: String,
-        required: true,
-        enum: ["user"]
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    timestamp: {
-        type: Date,
-        default: Date.now
-    }
+   name: {
+    type: String,
+    required: true
+   },
+   name: {
+    type: String,
+    required: true,
+    unique: true,
+   },
+   password: {
+    type: String,
+    required: true
+   },
+   credits: {
+    type: Number,
+    default: 20
+   },
 })
+
+// hashing password before saving
+userSchema.pre()
 
 const User = mongoose.model("User", userSchema)
 export default User;
--- server/routes/chatbot.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { message } from "../controllers/chatbot.controller.js";
-
-const chatbotRouter = express.Router();
-chatbotRouter.post("/message", message);
-chatbotRouter.get("/message", (req, res) => res.send("API Working"))
-export default chatbotRouter;
