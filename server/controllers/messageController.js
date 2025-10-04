11f3089a6186cbd4036092a4ef5eac53dea45091
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-04 16:57:31 +0500

    Enhance textMessageController: add OpenAI chat completion integration, improve message handling, and update user credits after message processing.

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,15 +1,44 @@
 import User from "../models/user.model.js";
 
-
 export const textMessageController = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const {chatId, prompt} = req.body;
+  try {
+    const userId = req.user._id;
+    const { chatId, prompt } = req.body;
 
-        const chat = await User.findOne({userId, _id: chatId})
-        chat.messages.push({role: "User", isImage: false, timestamp: Date.now(), content: prompt})
-        
-    } catch (error) {
-        
-    }
-}
+    const chat = await User.findOne({ userId, _id: chatId });
+    chat.messages.push({
+      role: "User",
+      isImage: false,
+      timestamp: Date.now(),
+      content: prompt,
+    });
+
+    const { choice } = await openai.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+
+    const reply = {
+      ...choice.messages[0],
+      isImage: false,
+      timestamp: Date.now(),
+    };
+
+    res.json({
+        success: true,
+        reply
+    })
+
+    chat.messages.push(replay)
+    await chat.save();
+
+    await User.updateOne({_id: userId}, {$inc: {credits: -1}})
+
+  } catch (error) {}
+};
