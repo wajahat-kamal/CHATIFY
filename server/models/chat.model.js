fc93a1104a08e24f09ae7b663d05bd9cd1324341
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-04 10:22:39 +0500

    Enhance chat model: add userId, userName, and messages fields to chatSchema, and enable timestamps for better tracking of chat entries.

--- server/models/chat.model.js
+++ server/models/chat.model.js
@@ -1,8 +1,32 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({
+const chatSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true, 
+    },
+    messages: [
+      {
+        isImage: { type: Boolean, default: false },
+        isPublished: { type: Boolean, default: false },
+        content: { type: String, required: true },
+        role: { type: String, required: true },
+        timestamp: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-})
-
-const Chat = mongoose.model("Chat", chatSchema)
-export default Chat
+const Chat = mongoose.model("Chat", chatSchema);
+export default Chat;
