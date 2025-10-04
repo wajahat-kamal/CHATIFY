9b5762779bcca5c0fbce22b987ae5b5f44bd54be
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-04 10:34:19 +0500

    Refactor createChat function: streamline chat creation logic, improve response structure, and enhance error handling in chatController.js.

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,23 +1,35 @@
 import Chat from "../models/chat.model.js";
 
+export const createChat = async (req, res) => {
+  try {
+    const userId = req.user._id;
 
-export const createChat = async (req, res)=> {
+    const chatData = {
+      userId,
+      userName: req.user.name,
+      name: "New Chat",
+      messages: [],
+    };
+
+    const newChat = await Chat.create(chatData);
+
+    return res.status(201).json({
+      success: true,
+      message: "Chat created successfully",
+      chat: newChat,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getChats = async (req, res) => {
     try {
-        const userId = req.user._id;
-
-        const chatData = {
-            userId,
-            userName: req.user.name,
-            name: "New Chat",
-            messages: []
-        }
-
-        await Chat.create(chatData)
-        res.status(200).json({success: true, message: "Chat created"})
+        
     } catch (error) {
-       return res.status(500).json({
-        success: false,
-        message: error.message
-       })  
-    } 
+        
+    }
 }
