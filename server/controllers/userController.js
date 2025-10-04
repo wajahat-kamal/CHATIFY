a909696b4b42f0f04c849b90376bdaab9160e131
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-04 09:09:16 +0500

    Refactor loginUser function: enhance error handling, streamline response structure, and ensure consistent error messages. Add getUser function to retrieve user details from the request.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,45 +41,51 @@ export const registerUser = async (req, res) => {
   }
 };
 
-
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(401).json({
-          success: false,
-          message: "Invalid email or password",
-        });
-      }
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(401).json({
-          success: false,
-          message: "Invalid email or password",
-        });
-      }
-  
-      const token = generateToken(user._id);
-  
-      return res.status(200).json({
-        success: true,
-        message: "Login successful",
-        token,
-        user: {
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          credits: user.credits, 
-        },
-      });
-    } catch (error) {
-      console.error("❌ Error in loginUser:", error.message);
-      return res.status(500).json({
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
         success: false,
-        message: "Server error, please try again later",
+        message: "Invalid email or password",
       });
     }
-  };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        credits: user.credits,
+      },
+    });
+  } catch (error) {
+    console.error("❌ Error in loginUser:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server error, please try again later",
+    });
+  }
+};
+
+export const getUser = (req, res) => {
+  try {
+    const user = req.user;
+    return res.status(200).json({ success: true, user });
+  } catch (error) {}
+};
